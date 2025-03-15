cfc40db5d1630e07c5f6afd9d7e870516568e7e9
StephanyCanuto <stephany.canutos25@gmail.com>
2025-03-15 17:08:33 -0300

    feat: Projeto Spring Boot com Grandle

---
+++ src/main/java/com/senaijava/senaijgradle/SenaijgradleApplication.java
@@ -0,0 +1,13 @@
+package com.senaijava.senaijgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaijgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaijgradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senaijava/senaijgradle/SenaijgradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senaijava.senaijgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaijgradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
